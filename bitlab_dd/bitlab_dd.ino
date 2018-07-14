91c96895c5317e492ac91ac6f1d45749aff2cd47
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-14 18:44:02 +0000

    NEW PROGRAM STRUCTURE - first iteration - just an empty .ino 	new file:   bitlab_dd.ino

---
+++ bitlab_dd/bitlab_dd.ino
@@ -0,0 +1,17 @@
+// Sat 14 Jul 18:35:52 UTC 2018
+
+// With the Arduino IDE, it's permissible to have an empty .ino
+// file to reference as the project's top-most source file,
+// and do everything else in .cpp or .h files (or any other
+// allowed file in the C++ programmer's environment).
+
+/*
+ *  files:
+ * 
+ *    bitlab_dd.ino
+ *    bitlab_ddsketch.cpp
+ *    bitlab_ddsketch.h
+ *    interpret_m4.cpp
+ * 
+ */
+
