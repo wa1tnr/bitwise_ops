a5555fef8dea47af9c9853f2eb9fcfe41261fe21
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-14 21:01:36 +0000

    Quick stash of currnt work. 	new file:   stashed/bitlab_dd.ino 	new file:   stashed/bitlab_ddsketch.cpp 	new file:   stashed/bitlab_ddsketch.h

---
+++ bitlab_dd/stashed/bitlab_ddsketch.cpp
@@ -0,0 +1,61 @@
+// Sat 14 Jul 20:21:17 UTC 2018
+// bitlab_ddsketch.cpp
+
+/* Charley Shattuck's
+
+   Tiny interpreter,
+   similar to myforth's Standalone Interpreter
+   This example code is in the public domain */
+
+// was:
+// bitlab_cc.ino
+
+// previous timestamp:
+// Sat 14 Jul 19:05:26 UTC 2018
+
+// target MCU: Adafruit Feather M0 Express
+
+#include <Arduino.h>
+#include "bitlab_ddsketch.h"
+
+/* Terminal Input Buffer for interpreter */
+const byte maxtib = 16;
+char tib[maxtib];
+
+byte pos;
+char ch;
+
+/* Incrementally read command line from serial port */
+byte reading(void) {
+    if (!Serial.available()) return 1;
+    ch = Serial.read();
+
+    Serial.print(ch); // printing();
+}
+
+/* Block on reading the command line from serial port */
+/* then echo each word */
+void readword() {
+    pos = 0;
+    tib[0] = 0;
+    while (reading());
+}
+
+void setup(void) {
+    Serial.begin(9600);
+    while (!Serial);
+    Serial.println ("Forth-like interpreter:");
+
+    Serial.println("Test message - signon.");
+    Serial.println("\r\n // bitlab_dd.ino ");
+    // Serial.print("\r\n\r\n END. "); // let them know there is no more.
+
+    // while(true);
+    // Serial.println("Line B."); // never reached
+}
+
+void loop(void) {
+    Serial.println("Line C -- feel free to type something, below.");
+    readword();
+}
+
---
+++ bitlab_dd/stashed/bitlab_ddsketch.h
@@ -0,0 +1,4 @@
+// Sat 14 Jul 19:05:26 UTC 2018
+// bitlab_ddsketch.h
+
+// intentionally empty - instantiate, only.
