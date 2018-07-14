58d4923001d9ccef287282466067e6adbd590be4
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-14 04:45:29 +0000

    Start serial port and wait for a connection. 	modified:   bitlab_aa.ino

--- bitlab_aa/bitlab_aa.ino
+++ bitlab_aa/bitlab_aa.ino
@@ -1,7 +1,13 @@
 // Sat 14 Jul 04:19:35 UTC 2018
 // bitlab_aa.ino
 
+// target MCU: Adafruit Feather M0 Express
+
 void setup(void) {
+    Serial.begin(9600);
+    while(!Serial) {
+        // do nothing
+    }
 }
 
 void loop(void) {
