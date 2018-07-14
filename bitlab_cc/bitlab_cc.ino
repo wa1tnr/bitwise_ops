71cb208c6bf056780fa19334e72a40629af75cc6
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-14 06:08:17 +0000

    Copy of bitlab_bb.ino, new instance here. 	new file:   bitlab_cc.ino

---
+++ bitlab_cc/bitlab_cc.ino
@@ -0,0 +1,78 @@
+// Sat 14 Jul 06:06:47 UTC 2018
+// bitlab_cc.ino
+
+// target MCU: Adafruit Feather M0 Express
+
+// any SAMD21 (M0+) or SAMD51 (M4) MCU will work; probably
+// AVR's as well.
+
+void example_A(void) {
+    int x = 3;
+    int n = 2;
+    int a = 0; // accumulator stand-in
+
+    Serial.println("\r\n Example A:\r\n");
+
+    Serial.print("  x = ");
+    Serial.println(x);
+
+    Serial.print("  n = ");
+    Serial.println(n);
+
+    a = x << n;
+
+    Serial.println("");
+
+    Serial.println("  a = x << n");
+    // Serial.println("  <--- suppressed operation ");
+
+    Serial.print("  a = ");
+    Serial.println(a);
+}
+
+void example_B(void) {
+    int x = 3; // seed value, our 'interesting number' to be modified
+    int n = 5; // powers of two -- how many places to the left we bit-shift 'x'
+    int a = 0; // accumulator stand-in
+
+    Serial.println("\r\n Example B:\r\n");
+
+    Serial.print("  x = ");
+    Serial.println(x);
+
+    Serial.print("  n = ");
+    Serial.println(n);
+
+    a = x << n;
+
+    Serial.println("");
+
+    Serial.println("  a = x << n");
+    // Serial.println("  <--- suppressed operation ");
+
+    Serial.print("  a = ");
+    Serial.println(a);
+}
+
+void setup(void) {
+    Serial.begin(9600);
+    while(!Serial) {
+        // do nothing
+    }
+
+    Serial.println("\r\n // bitlab_cc.ino ");
+    example_A();
+    delay(7000); // nice long delay
+    example_B();
+
+    Serial.print("\r\n\r\n END. "); // let them know there is no more.
+    while(true) {
+    // do nothing
+    }
+    Serial.println("Line B."); // never reached
+}
+
+void loop(void) {
+    Serial.println("Line C.");
+}
+
