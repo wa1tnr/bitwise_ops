468a8fd4f7d2da32d4dd9010cc00b02d564fe1fd
wa1tnr <wa1tnr@users.noreply.github.com>
2018-07-14 04:49:05 +0000

    Setup three variables (x, n and a) and print the sign-on message. 	modified:   bitlab_aa.ino

--- bitlab_aa/bitlab_aa.ino
+++ bitlab_aa/bitlab_aa.ino
@@ -1,13 +1,21 @@
-// Sat 14 Jul 04:19:35 UTC 2018
+// Sat 14 Jul 04:47:39 UTC 2018
 // bitlab_aa.ino
 
 // target MCU: Adafruit Feather M0 Express
 
+// any SAMD21 (M0+) or SAMD51 (M4) MCU will work; probably
+// AVR's as well.
+
 void setup(void) {
+    int x = 3;
+    int n = 2;
+    int a = 0; // accumulator stand-in
+
     Serial.begin(9600);
     while(!Serial) {
         // do nothing
     }
+    Serial.println("\r\n Example A:\r\n");
 }
 
 void loop(void) {
