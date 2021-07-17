bbedef7bfc84735cc2480ad315f0d8595f040466
wa1tnr <wa1tnr@users.noreply.github.com>
2021-07-17 12:51:06 +0000

    New Series - bitlab p ItsyBitsy 32u4 5V 16 MHz
    	new file:   p_bitlab_aa.ino
    
    On branch testing-ab

---
+++ p_bitlab_aa/p_bitlab_aa.ino
@@ -0,0 +1,18 @@
+// p_bitlab_aa.ino
+// Sat Jul 17 12:17:13 UTC 2021
+
+// Target:
+//     5V 16 MHz ItsyBitsy 32u4 from Adafruit
+//     https://www.adafruit.com/product/3677
+//     https://learn.adafruit.com/introducting-itsy-bitsy-32u4/
+
+// IDE:
+//     Adafruit AVR Boards (in Boards Manager)
+
+// intentionally empty .ino file.
+
+// - see the sketch file for active code:
+
+//     n_bitlab_aa_sketch.cpp
+
+// END.
