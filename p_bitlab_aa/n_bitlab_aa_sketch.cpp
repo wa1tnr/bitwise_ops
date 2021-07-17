3e95d9a2d47c92ffe8c95d6c1f54ed5a03b758d0
wa1tnr <wa1tnr@users.noreply.github.com>
2021-07-17 13:01:48 +0000

    blinkenlight working 1 Hz

--- p_bitlab_aa/n_bitlab_aa_sketch.cpp
+++ p_bitlab_aa/n_bitlab_aa_sketch.cpp
@@ -8,14 +8,25 @@ void init_serial(void) {
 }
 
 void init_gpio(void) {
+    pinMode(LED_BUILTIN, 1); // OUTPUT
+}
+
+bool pin_state;
+
+void cpl(int pin) {
+    pin_state = digitalRead(LED_BUILTIN);
+    pin_state = !pin_state;
+    digitalWrite(LED_BUILTIN, pin_state);
 }
 
 void blink(void) {
+    cpl(LED_BUILTIN);
     delay(500);
 }
 
 void setup(void) {
     // gpio serial init
+    init_gpio();
 }
 
 void loop(void) {
