7fd70c7ba407f4461bdaadffee79d33c1663fc6e
wa1tnr <wa1tnr@users.noreply.github.com>
2021-07-28 16:56:11 +0000

    First meaningful commit - test loop w/ERROR forced.   * include Arduino.h for the first time.   * start serial   * print when serial ready   * do something in the loop   * comment out correct loop while test   * test fall-through condition after a 'wrong' while   * print test result after a delay (demonstrates loop works)
    	modified:   n_forth-avr-a.cpp
    
    On branch testing-ab

--- forth_aa/forth_avr-a/n_forth-avr-a.cpp
+++ forth_aa/forth_avr-a/n_forth-avr-a.cpp
@@ -1,3 +1,14 @@
 // Wed Jul 28 16:41:52 UTC 2021
-void setup(void) { }
-void loop(void) { }
+#include <Arduino.h>
+void setup(void) {
+    Serial.begin(115200);
+    while(!Serial);
+    delay(3000);
+    Serial.println("here.");
+}
+void loop(void) {
+    // while(-1);
+    while(0);
+    delay(5000);
+    Serial.println("Fall-through.  ERROR.");
+}
