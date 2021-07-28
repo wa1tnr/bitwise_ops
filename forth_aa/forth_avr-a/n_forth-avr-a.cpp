bd1251130895b22fde0533907cfebdb9cc50983c
wa1tnr <wa1tnr@users.noreply.github.com>
2021-07-28 17:21:04 +0000

    Charley's code terms
      * Add license-like comment
      * define and print the date stamp
    
    	modified:   n_forth-avr-a.cpp
    
    On branch testing-ab

--- forth_aa/forth_avr-a/n_forth-avr-a.cpp
+++ forth_aa/forth_avr-a/n_forth-avr-a.cpp
@@ -1,10 +1,16 @@
-// Wed Jul 28 16:41:52 UTC 2021
+#define DATESTAMP "Wed Jul 28 17:19:27 UTC 2021"
+
+/* Includes Charley Shattuck's Tiny interpreter,
+   similar to myforth's Standalone Interpreter
+   Charley's example code is in the public domain */
+
 #include <Arduino.h>
 void setup(void) {
     Serial.begin(115200);
     while(!Serial);
     delay(3000);
     Serial.println("here.");
+    Serial.println(DATESTAMP);
 }
 void loop(void) {
     Serial.println("Entering loop.  LAST message, EVER, seen.");
@@ -13,3 +19,5 @@ void loop(void) {
     delay(5000);
     Serial.println("Fall-through.  ERROR.");
 }
+
+// END.
