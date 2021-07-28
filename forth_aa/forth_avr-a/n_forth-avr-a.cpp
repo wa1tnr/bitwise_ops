8313919dd29b9e7edb5a4977b2d1f3e6900c2ce6
wa1tnr <wa1tnr@users.noreply.github.com>
2021-07-28 18:09:18 +0000

    blink hooks added

--- forth_aa/forth_avr-a/n_forth-avr-a.cpp
+++ forth_aa/forth_avr-a/n_forth-avr-a.cpp
@@ -1,13 +1,19 @@
-#define DATESTAMP "Wed Jul 28 17:19:27 UTC 2021"
+#define DATESTAMP "Wed Jul 28 18:08:10 UTC 2021"
 
 /* Includes Charley Shattuck's Tiny interpreter,
    similar to myforth's Standalone Interpreter
    Charley's example code is in the public domain */
 
 #include <Arduino.h>
+
+void setup_gpio(void) { }
+
+void blink(void) { }
+
 void setup(void) {
+    setup_gpio();
     Serial.begin(115200);
-    while(!Serial);
+    while(!Serial) { blink(); }
     delay(3000);
     Serial.println("here.");
     Serial.println(DATESTAMP);
