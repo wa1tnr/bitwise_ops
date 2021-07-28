0f798745b906540e7727ac0e1557d24ec8972ef1
wa1tnr <wa1tnr@users.noreply.github.com>
2021-07-28 18:19:37 +0000

    init_gpio cpl delayed init_serial and print the LED pin assignment
    Functions are mostly skeletal
    
    	modified:   n_forth-avr-a.cpp
    
    On branch dvlp-aa

--- forth_aa/forth_avr-a/n_forth-avr-a.cpp
+++ forth_aa/forth_avr-a/n_forth-avr-a.cpp
@@ -1,4 +1,4 @@
-#define DATESTAMP "Wed Jul 28 18:08:10 UTC 2021"
+#define DATESTAMP "Wed Jul 28 18:17:44 UTC 2021"
 
 /* Includes Charley Shattuck's Tiny interpreter,
    similar to myforth's Standalone Interpreter
@@ -6,19 +6,46 @@
 
 #include <Arduino.h>
 
-void setup_gpio(void) { }
+void init_gpio(void) {
+    pinMode(LED_BUILTIN, 1);
+    digitalWrite(LED_BUILTIN, 0);
+}
 
-void blink(void) { }
+bool pin_state;
 
-void setup(void) {
-    setup_gpio();
+int wasted;
+
+void cpl(int pin) {
+    int test_del = pin;
+    wasted = test_del;
+    // pin_state = digitalRead(LED_BUILTIN);
+    // pin_state = !pin_state;
+    // digitalWrite(LED_BUILTIN, pin_state);
+}
+
+void delayed(void) {
+}
+
+void blink(void) {
+    cpl(LED_BUILTIN);
+    delayed(); // delay(500);
+}
+
+void init_serial(void) {
     Serial.begin(115200);
     while(!Serial) { blink(); }
+}
+
+void setup(void) {
+    init_gpio();
+    init_serial();
     delay(3000);
     Serial.println("here.");
     Serial.println(DATESTAMP);
 }
 void loop(void) {
+    Serial.print("__LED_BUILTIN__ ");
+    Serial.println(wasted); // temporary - delete me soon
     Serial.println("Entering loop.  LAST message, EVER, seen.");
     while(-1);
     // while(0);
