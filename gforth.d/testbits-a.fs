0129dd803a749439a5f25d14915f23aa0e779a00
wa1tnr <wa1tnr@users.noreply.github.com>
2023-02-17 16:31:48 +0000

    possibly good commit with bad comments

--- gforth.d/testbits-a.fs
+++ gforth.d/testbits-a.fs
@@ -27,21 +27,56 @@ decimal
 : set5? ( n -- BOOL )
   abs $20 AND $20 = ;
 
+: set4? ( n -- BOOL )
+  abs $10 AND $10 = ;
+
+: set3? ( n -- BOOL )
+  abs $08 AND $08 = ;
+
+: set2? ( n -- BOOL )
+  abs $04 AND $04 = ;
+
+: set1? ( n -- BOOL )
+  abs $02 AND $02 = ;
+
+: set0? ( n -- BOOL )
+  abs $01 AND $01 = ;
+
 binary
 
 : prebin ( n -- ? ) \ no SED given
-  dup set7?  dup IF ." MSB is set! " cr drop drop drop exit THEN
-  drop ." MSB is clr! " cr
+  dup set7?  dup IF ." bit 7 is set!   MSB is set! " cr drop drop exit THEN
+  drop ." bit 7 is clr!   MSB is clr! " cr
 
   dup set6?  dup IF ." bit 6 is set! " cr drop drop exit THEN
   drop ." bit 6 is clr! " cr
   
   dup set5?  dup IF ." bit 5 is set! " cr drop drop exit THEN
   drop ." bit 5 is clr! " cr
+
+
+  dup set4?  dup IF ." bit 4 is set! " cr drop drop exit THEN
+  drop ." bit 4 is clr! " cr
+
+  dup set3?  dup IF ." bit 3 is set! " cr drop drop exit THEN
+  drop ." bit 3 is clr! " cr
+
+  dup set2?  dup IF ." bit 2 is set! " cr drop drop exit THEN
+  drop ." bit 2 is clr! " cr
+
+  dup set1?  dup IF ." bit 1 is set! " cr drop drop exit THEN
+  drop ." bit 1 is clr! " cr
+
+  dup set0?  dup IF ." bit 0 is set! " cr drop drop exit THEN
+  drop ." bit 0 is clr! " cr
+  cr
+  ." is there underflow yet or does this line print prior " cr
+  drop
   cr ." control reaches end of prebin.  exiting. check the stack. " cr
 ;
 
 : test ( n -- dont care)
+  cr
   depth 1 < IF ." bad" EXIT THEN
   prebin
 ;
